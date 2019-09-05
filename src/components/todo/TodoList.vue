4e1aad7c38dd3ba217b0a63d2b9ff8e0e83b63a2
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-09-05 16:02:41 +0200

    Adds Todo component and loads them dynamicaly

---
+++ src/components/todo/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>
+            Test
+        </h1>
+       
+        <Todo text="test"></Todo>
+        <Todo 
+              v-for="todo in todos"
+              v-bind:key= todo.id
+              v-bind:title= todo.text
+              ></Todo>
+    </div>
+</template>
+
+<script>
+    import Todo from './Todo.vue'
+    export default {
+        name: "TodoList",
+        components: {
+            Todo
+        },
+        data: function () {
+            return {
+                todos: [
+                    { id: 1, text: "Task 1" },
+                    { id: 2, text: "Task 2" }
+                ]
+            }
+        }
+    }
+</script>
+
+<style></style>
